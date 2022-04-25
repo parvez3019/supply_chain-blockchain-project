29fb96fcd47907496d467bdc883cb65dcd77c2ce
parvez3019 <pha3019@gmail.com>
2022-04-25 15:30:20 +0530

    Update notes with rinkeby contract deployments

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,10 +1,25 @@
+const HDWallet = require('truffle-hdwallet-provider');
+
+const mnemonic = ""
+const infuraKey = ""
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+
+    rinkeby: {
+      provider: function () {
+        return new HDWallet(mnemonic, "https://rinkeby.infura.io/v3/" + infuraKey);
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
+
   },
   compilers: {
     solc: {
